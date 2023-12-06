ed86e3d4b161932940e5922d69b03b5deb1090b4
Artyom Novoselov <an@versta24.ru>
2023-12-06 16:00:33 +0100

    fix: catch errors in counter

--- src/utils/count-pages.js
+++ src/utils/count-pages.js
@@ -1,12 +1,13 @@
 // GitHub doesn't send total amount of results but it has link to
 // last in header which has the last page number.
 export const countPages = (headers) => {
-    const header = headers.get('Link')
-
-    if (!header) return 1
-
-    const lastLink = header.split(',').find((h) => h.includes('last'))
-    const pageCount = lastLink.match(/&page=\d*/gm)[0]
-    const totalPages = parseInt(pageCount.match(/\d*/gm).find((m) => !!m))
-    return totalPages
+    try {
+        const header = headers.get('Link')
+        const lastLink = header.split(',').find((h) => h.includes('last'))
+        const pageCount = lastLink.match(/&page=\d*/gm)[0]
+        const totalPages = parseInt(pageCount.match(/\d*/gm).find((m) => !!m))
+        return totalPages
+    } catch {
+        return 1
+    }
 }
