83db630e6d3e26ecddc25b23025ab95fb324cc85
Artyom Novoselov <an@versta24.ru>
2023-12-06 11:29:41 +0100

    feat: repository component

---
+++ src/components/molecules/repository/repository.jsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx'
+
+import s from './repository.module.css'
+import Link from '../../atoms/link'
+import Badge from '../../atoms/badge'
+import TruncatedText from '../truncated-text'
+
+export default function Repository({ repo }) {
+    const {
+        name,
+        repoName,
+        user: { username },
+    } = repo
+
+    return (
+        <div className={clsx('content', s.container)}>
+            <Link href={`/repository/${name}`}>
+                <h3>
+                    <TruncatedText content={name} />
+                </h3>
+            </Link>
+            <Link href={`/${username}`}>
+                <Badge>{username}</Badge>
+            </Link>
+        </div>
+    )
+}
